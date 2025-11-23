176242efa56c60e65bab5d019b8a81cbf78e603a
Timothy Jordy Weley <tmthyjw@gmail.com>
2025-11-23 09:38:22 +0800

    feat: Making Main App and each section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-//Component adalah function yg return JSX
-//JSX hanya boleh memiliki 1 parent element
-import React, { useState } from "react";
-import HelloWorld from "./HelloWorld";
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
 
-const App = () => {
-  // let hello = "Hello World"; // Ini variabel biasa
-  const [hello, setHello] = useState("Hello World!!!"); //Ini variabel state
+const MainLayout = () => (
+  <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden bg-white dark:bg-gray-900 transition-colors duration-300">
+    <Navbar />
+    <main className="grow pt-16 md:pt-20">
+      <Hero />
+      <Features />
+      <Testimonials />
+    </main>
+    <Footer />
+  </div>
+);
 
-  const handleClick = () => {
-    console.log("button di klik");
-    // hello = "Selamat Datang"; //ini cara merubah variabel biasa
-    setHello("Selamat Datang"); //ini cara merubah variabel state
-    console.log(hello);
-  };
+function App() {
   return (
-    <>
-      <div className="text-[50px] bg-blue-500 m-10 p-10 font-serif">
-        {hello}
-      </div>
-      <HelloWorld
-        text="This is a child component 1"
-        handleClick={handleClick}
-      />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleClick}
-      >
-        Click Me
-      </button>
-    </>
+    <Router>
+            <Routes>
+              <Route path="/" element={<MainLayout />}>
+              </Route>
+            </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import RestAPI from "./RestAPI.jsx";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
-    {/* <App /> */}
-    <RestAPI />
+    <App />
   </StrictMode>
 );
